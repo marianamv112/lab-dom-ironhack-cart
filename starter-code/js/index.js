0a80dbacde304c1532b1a2afa1f88e724da220f2
marianamv112 <marianamv112@gmail.com>
2019-06-10 19:26:15 +0100

    LIS Mariana 98% - Some CSS is missing, I believe I should've used tables

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,64 +1,108 @@
-function deleteItem(e){
+function deleteItem(e) {
+  
+  let container = e.currentTarget.closest('section');
+  console.log(container);
+  container.innerHTML = '';
+}
+
+function getPriceByProduct(itemNode) {
 
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
+  let input = document.getElementsByClassName('quantity');
+  let unitCost = document.getElementsByClassName("unitCost");
+  let subtotalPrice = document.getElementsByClassName("subtotalPrice");
+  let totalPrice = document.getElementsByClassName("totalPrice");
+
+  let sumOfPrices = 0;
+
+  for (let i = 0; i < unitCost.length; i++) {
+    if (input[i].value) {
+      let subTotalValue = (input[i].value * Number(unitCost[i].innerText.substring(1))).toFixed(2);
+      subtotalPrice[i].innerText = '$' + subTotalValue;
+      sumOfPrices += Number(subTotalValue);
+    }
+  }
+  totalPrice[0].innerText = '$' + sumOfPrices;
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(e, itemName, itemUnitPrice) {
+  let referenceContainer = e.currentTarget.closest('section');
+
+  let newContainer = document.createElement('section');
+  newContainer.className = 'wrapper';
+
+  newContainer.innerHTML +=
+    `<div class="productName">
+  <span>${itemName}</span>
+</div>
+<div class="unitCost">
+  <span>$${itemUnitPrice}</span>
+</div>
+<div class="inputQty">
+  <label for="input">QTY</label>
+  <input type="number" class="quantity">
+</div>
+<div class="subtotal">
+  <span class="subtotalPrice">$0.00</span>
+</div>
+<div>
+  <button class="btn-delete">Delete</button>
+</div>`;
+
+  referenceContainer.before(newContainer);
+  
+  window.onload();
+  
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem(e) {
+  let newItemName = document.getElementById('nem-item-name');
+  let newItemPrice = document.getElementById('new-item-price');
+  
+  createNewItemRow(e, newItemName.value, newItemPrice.value);
+
+  console.log(newItemName, newItemPrice);
+
+  newItemName.value = "";
+   newItemPrice.value = "";
+
 
 }
 
-function createNewItem(){
-
-}
-
-window.onload = function(){
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
+
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
-
-  calculatePriceButton.onclick = function() {
-    let input = document.querySelector('input');
-    let unitCost = document.getElementsByClassName("unitCost");
-    let totalPrice = document.getElementsByClassName("totalPrice");
-  
-    if (input.value) {
-      totalPrice[0].innerText = '$' + (input.value * Number(unitCost[0].innerText.substring(1))).toFixed(2);
-      
-    }
-  }
-
 };
